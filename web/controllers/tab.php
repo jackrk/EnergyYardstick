bf15bc9c5da83c13b29590544b28794bf2aabacf
jackrk <kohn.jack@yahoo.com>
2014-03-06 00:29:17 -0600

    Added fade in/out effect on main content container when changing tabs.
    Controller now responsible for loading tab contents instead of direct calls. This will allow us to make queries and execute all of our php inside the controller when switching tabs, which is the main purpose of using the cjax MVC functionality.

--- web/controllers/tab.php
+++ web/controllers/tab.php
@@ -6,16 +6,23 @@
  * Time: 11:05 PM
  */
 
-class tab_selector {
+class controller_tab {
     function rating() {
+        $ajax = ajax();
 
+        usleep(400000);
+        $ajax->call("../php/tabs/rating_main.php", "tab_container");
     }
 
     function compare() {
-
+        $ajax = ajax();
+        usleep(400000);
+        $ajax->call("../php/tabs/compare_main.php", "tab_container");
     }
 
     function history() {
-
+        $ajax = ajax();
+        usleep(400000);
+        $ajax->call("../php/tabs/history_main.php", "tab_container");
     }
-} 
+}
