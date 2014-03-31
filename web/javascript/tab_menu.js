65f8d4c89b94222d38cc8a755f9c636e3cce30e8
jackrk <kohn.jack@yahoo.com>
2014-03-30 21:21:30 -0500

    Full commit of rating_tab to master
    Couldn't get merge and pull request to work right so I'm manually
    committing the full changeset from my layout work in rating_tab branch
    to master.

--- web/javascript/tab_menu.js
+++ web/javascript/tab_menu.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
+
+
+
     if (!$.support.transition)
         $.fn.transition = $.fn.animate;
     $(".tab_link").mouseenter(function(){
-        $(this).transition({backgroundColor: 'lightgreen', queue: false}, 200);
-        $(this).bind('mouseout', function(){
-            $(this).transition({backgroundColor: 'white', queue: false}, 150);
-            $(this).unbind('mouseout');
+        $(this).transition({color: 'white', backgroundColor: 'darkslategray', queue: false}, 100);
+        $(this).bind('mouseleave', function(){
+            $(this).transition({color: '', backgroundColor: '', queue: false}, 100);
+            $(this).unbind('mouseleave');
         });
     });
+
+    $("#tips_submit").mouseenter(function(){
+       $(this).transition({backgroundColor: '#6AA8BF', queue: false}, 200);
+        $(this).bind('mouseleave', function(){
+            $(this).transition({backgroundColor: '#47707F', queue: false}, 200);
+            $(this).unbind('mouseleave');
+        });
+    });
+
     /*$(".tab_link").bind('click', function() {
         $("#tab_container").transition({opacity: 0, queue: false}, 200);
         setTimeout(function(){
@@ -15,24 +27,65 @@ $(document).ready(function() {
         },520);
     });*/
     $("#tab_rating").click(function(){
+        if (!$(this).hasClass('tab_active')) {
+            $(this).parent().children().each(function() {
+                $(this).removeClass("tab_active");
+            });
+            $(this).addClass("tab_active");
+        }
         $("#tab_container").load("../php/tabs/rating_main.php", function() {
+            var hid = $("#hidden_rating_number").html();
+            if (hid == "")
+                $("#hidden_rating_number").html($(".rating_number").html());
+            else
+                $('.rating_number').html(hid);
+            var curval = parseInt($("#hidden_rating_number").html());
+            animateRating($("#rating_container"), $(".rating_text"), curval, 10);
             $('.rating_slider').noUiSlider({
-                range: [1, 10],
-                start: 5,
+                range: [curval, 10],
+                start: curval,
                 handles: 1,
                 step: 1,
-                orientation: "horizontal"
+                orientation: "horizontal",
+                direction: "ltr",
+                behaviour: 'extend-tap',
+                serialization: {
+                    resolution: 1
+                },
+                slide: function() {
+                    var offset = parseInt($("#slider").val());
+                    $(".rating_number").html(offset);
+                    $("#hidden_rating_number").html(offset);
+                    var $rating_background = $("#rating_container");
+                    animateRating($rating_background, $(".rating_text"), offset, 200);
+
+                }
             });
+
+
         });
     });
     $("#tab_compare").click(function(){
+        if (!$(this).hasClass('tab_active')) {
+            $(this).parent().children().each(function() {
+                $(this).removeClass("tab_active");
+            });
+            $(this).addClass("tab_active");
+        }
         $("#tab_container").load("../php/tabs/compare_main.php", function() {
 
         });
     });
     $("#tab_history").click(function(){
+        if (!$(this).hasClass('tab_active')) {
+            $(this).parent().children().each(function() {
+                $(this).removeClass("tab_active");
+            });
+            $(this).addClass("tab_active");
+        }
         $("#tab_container").load("../php/tabs/history_main.php", function() {
 
         });
     });
+    $('#tab_rating').click();
 });
--- web/php/tabs/history_main.php
+++ web/php/tabs/history_main.php
@@ -6,4 +6,15 @@
  * Time: 11:28 PM
  */
 ?>
-<div id="history_container">HISTORY</div>
+<script type="text/javascript" src="/javascript/range_selector.js"></script>
+<div id="history_container">
+    <div id="histgraph_container">
+
+    </div>
+    <div id="range_buttons">
+        <div style="width: 199px !important" class="range_button selected">
+            12<span class="range_month">months</span></div><span class="range_button">
+            24<span class="range_month">months</span></span><span class="range_button">
+            36<span class="range_month">months</span></span>
+    </div>
+</div>
