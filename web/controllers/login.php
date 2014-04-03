7388b028967d580a0959d4fe6f7f103a6d67312a
jackrk <kohn.jack@yahoo.com>
2014-04-02 20:39:47 -0500

    syncing workspaces

--- web/controllers/login.php
+++ web/controllers/login.php
@@ -8,7 +8,21 @@
 
 class controller_login
 {
-    function authenticate($user, $password) {
+    function authenticate($form_fields) {
+        $ajax = ajax();
 
+        //$ajax->alert("Server Says....\n\nFields submitted: \n".print_r($form_fields,1));
+        $ajax->alert("Logged in as: " . $form_fields[username]);
+
+        $ajax->location("/php/home.php");
+
+        /*if(!$username) {
+            return $ajax->err = 'Enter your username';
+        }
+        if(!$password) {
+            return $ajax->err = 'Enter your password';
+        }*/
+        //$ajax->success("Logged in as: ".$username);
+        //header("Location: /php/home.php");
     }
 }
