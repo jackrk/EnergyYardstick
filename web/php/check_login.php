2cfcec10f0cc7769159cdc826e2bbdeb3cc37e8b
Bilal Besic <bbesic@iastate.edu>
2014-10-09 14:05:23 -0500

    login
    working

--- web/php/check_login.php
+++ web/php/check_login.php
@@ -17,8 +17,7 @@ $password = stripslashes($password);
 $username = mysql_real_escape_string($username);
 $password = mysql_real_escape_string($password);
 
-
-$sql = "SELECT * FROM $tbl_name WHERE username = '$username' AND password = '$password'";
+$sql = "select * from $tbl_name where username = '$username' and password = '$password'";
 $result = mysql_query($sql, $link) or die ('Unable to run query:'.mysql_error());
 
 $count = mysql_num_rows($result);
@@ -26,19 +25,19 @@ $count = mysql_num_rows($result);
 $row = mysql_fetch_row($result);
 $questions = $row[2];
 
-if($count == 1) {
-    if ($questions = 1) {
-        header('Location= thecityofames.org/php/rating_main.php');
+if($count == 1){
+    if($questions == 1){
+        header("Location: rating_main.php");
     }
     else
-        header('Location= thecityofames.org/php/questions.php');
+        header("Location: questions.php");
 }
 
-else {
-    $message = 'Wrong Username or Password';
+else{
+    $message = "Wrong usernamd or password";
     echo "<script type='text/javascript'> alert('$message'); </script>";
 
-    header("Refresh: 0; URL = thecityofames.org/php/login.php");
+    header("Refresh:0; URL= login.php");
 }
 
 ?>
--- web/php/login.php
+++ web/php/login.php
@@ -5,11 +5,11 @@
  * Date: 4/1/14
  * Time: 11:04 AM
  */
-require_once "../ajax.php";
+//require_once "../ajax.php";
 
-$ajax = ajax();
+//$ajax = ajax();
 
-$ajax->click("ajaxsubmit",$ajax->form('../ajax.php/login/authenticate'));
+//$ajax->click("ajaxsubmit",$ajax->form('../ajax.php/login/authenticate'));
 /*
 $ajax->call("../ajax.php?tab/rating");
 
@@ -19,7 +19,7 @@ $ajax->click("tab_history",$ajax->call("../ajax.php?tab/history")); */
 
 <!DOCTYPE html>
 <head>
-    <?php echo $ajax->init(); ?>
+
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
     <link href="../css/login.css" rel="stylesheet" type="text/css">
 </head>
@@ -28,9 +28,9 @@ $ajax->click("tab_history",$ajax->call("../ajax.php?tab/history")); */
 
 </div>
 <div id="logincontainer">
-    <form id="loginform" class="formlogin" style="background-color:#FFFFFF;font-size:12px;font-family:'Open Sans',sans-serif;font-weight:300;color:#666666;max-width:480px;min-width:150px" method="post" action="" onsubmit="return false;"><div class="logintitle">Welcome to the Ames Energy Yardstick</div>
-        <div class="element-input" ><label class="title">User</label><input class="inputbox" type="text" id="a[username]" name="a[username]" placeholder="username" /></div>
-        <div class="element-input" ><label class="title">Password</label><input class="inputbox" type="password" id="a[password]" name="a[password]" placeholder="password" /></div>
+    <form id="loginform" class="formlogin" style="background-color:#FFFFFF;font-size:12px;font-family:'Open Sans',sans-serif;font-weight:300;color:#666666;max-width:480px;min-width:150px" method="post" action="check_login.php"><div class="logintitle">Welcome to the Ames Energy Yardstick</div>
+        <div class="element-input" ><label class="title">User</label><input class="inputbox" type="text" id="a[username]" name="username" placeholder="username" /></div>
+        <div class="element-input" ><label class="title">Password</label><input class="inputbox" type="password" id="a[password]" name="password" placeholder="password" /></div>
 
         <div class="submit"><div id="err"></div><input id="ajaxsubmit" name="ajaxsubmit" class="submit_button" type="submit" value="Submit"/></div>
     </form>
