25d46821ab21eb483ae0f54342ec1413654cfd82
Bilal Besic <bbesic@iastate.edu>
2014-10-30 15:53:08 -0500

    questions page
    working with 6 new questions picked from alyssa’s list. We can discuss
    whether or not these are the questions that we want, but it is hooked
    up with the db for now.

--- web/controllers/questions.php
+++ web/controllers/questions.php
@@ -7,21 +7,39 @@
  */
 class controller_questions
 {
+    /**
+     * @param $form_fields
+     */
     function authenticate($form_fields) {
+        session_start();
         $ajax = ajax();
-       /* if(!$form_fields[username]) {
-            return $ajax->err = 'Enter your username';
-        }
-        if(!$form_fields[password]) {
-            return $ajax->err = 'Enter your password';
-        }*/
-        //$ajax->alert("Server Says....\n\nFields submitted: \n".print_r($form_fields,1));
-        //$ajax->alert("Logged in as: " . $form_fields[username]);
 
-        $ajax->location("/php/home.php");
+        $host = "69.195.124.206";
+        $user = "theciuc0_jdev";
+        $pass = "tqHzLt6N]h8X";
+        $db_name = "theciuc0_1";
+        $tbl_name = "Initial_Questions";
 
+        $link = mysql_connect("$host", "$user", "$pass") or die("cannot connect");
+        mysql_select_db("$db_name") or die("cannot select DB");
 
-        //$ajax->success("Logged in as: ".$username);
-        //header("Location: /php/home.php");
+        $username = $_SESSION ['username'];
+        $house = $_SESSION['house_id'];
+        $heating = $form_fields[heating_system];
+        $space_heaters = $form_fields[space_heater];
+        $cooling_system = $form_fields[cooling_system];
+        $lighting = $form_fields[lighting];
+        $thermostat = $form_fields[thermostat];
+        $water_heater = $form_fields[water_heater];
+
+        $sql = "INSERT INTO $tbl_name (heating, space_heaters, cooling_system, bulb_type, prog_thermostat, water_heater, username)
+                VALUES('$heating', '$space_heaters', '$cooling_system', '$lighting', '$thermostat', '$water_heater', '$username')";
+
+        $sql2 = "UPDATE User_House SET questions=1 WHERE username = '$username' and house_id = '$house'";
+
+        mysql_query($sql);
+        mysql_query($sql2);
+
+        $ajax->location("/php/rating_main.php");
     }
 }
--- web/php/questions.php
+++ web/php/questions.php
@@ -9,7 +9,11 @@ require_once "../ajax.php";
 
 $ajax = ajax();
 
-$ajax->click("q_ajaxsubmit",$ajax->form('../ajax.php/questions/authenticate'));
+if (isset($_SESSION['username'])) {
+    $ajax->click("q_ajaxsubmit",$ajax->form('../ajax.php/questions/authenticate'));
+} else {
+    header("Location: login.php");
+}
 
 ?>
 <!DOCTYPE html>
@@ -26,38 +30,44 @@ $ajax->click("q_ajaxsubmit",$ajax->form('../ajax.php/questions/authenticate'));
 <div id="logincontainer">
     <form id="questionform" class="questionform" method="post" action="" onsubmit="return false;"><div class="logintitle">Tell us a little about your home</div>
         <div class="question">
-            <span>Do you have an <b>electric water heater</b></span>
-            <span id="a_water_heat" class="answertext">NO</span>
-            <input id="q_water_heat" type="checkbox" class="answer cb_answer"/>
+            <span>Is your <b>heating system</b> gas or electric?</span>
+            <span id="a_heating_system" class="heating_system"></span>
+            <select id="q_heating_system" name="a[heating_system]" class="answer sl_answer">
+                <option>Gas</option>
+                <option>Electric</option>
+            </select>
         </div>
         <div class="question">
-            <span>Do you have an <b>electric furnace</b></span>
-            <span id="a_furnace" class="answertext">NO</span>
-            <input id="q_furnace" type="checkbox" class="answer cb_answer"/>
+            <span>Do you use <b>electric space heaters</b> for supplemental heat?</span>
+            <span id="a_space_heater" class="answertext">NO</span>
+            <input id="q_space_heater" name="a[space_heater]" type="checkbox" class="answer cb_answer"/>
         </div>
         <div class="question">
-            <span>Do you have <b>electric baseboard heating</b></span>
-            <span id="a_baseboard" class="answertext">NO</span>
-            <input id="q_baseboard" type="checkbox" class="answer cb_answer"/>
+            <span>Do you have, and use, your <b>cooling system?</b></span>
+            <span id="a_cooling_system" class="answertext">NO</span>
+            <input id="q_cooling_system" name="a[cooling_system]" type="checkbox" class="answer cb_answer"/>
         </div>
         <div class="question">
-            <span>What <b>type of lighting</b> do you predominately have</span>
+            <span>What <b>type of lighting </b>do you predominately have</span>
             <span id="a_lighting" class="lightimg"></span>
-            <select id="q_lighting" class="answer sl_answer">
+            <select id="q_lighting" name="a[lighting]" class="answer sl_answer">
                 <option>Incandescent</option>
                 <option>Compact Fluorescent</option>
                 <option>LED</option>
             </select>
         </div>
         <div class="question">
-            <span>Do you have a <b>fridge older than 1990</b></span>
-            <span id="a_fridge" class="answertext">NO</span>
-            <input id="q_fridge" type="checkbox" class="answer cb_answer"/>
+            <span>Do you have a <b>programmable thermostat?</b></span>
+            <span id="a_thermostat" class="answertext">NO</span>
+            <input id="q_thermostat" name="a[thermostat]" type="checkbox" class="answer cb_answer"/>
         </div>
         <div class="question">
-            <span><b>How many people</b> live in your household</span>
-            <input type="number" name="q_people" class="answer txt_answer" min="1" max="10" />
-            <!--<input id="q_people" type="text" class="answer txt_answer"/>-->
+            <span>Is your <b>water heater</b> gas or electric?</span>
+            <span id="a_water_heater" class="water_heater"></span>
+            <select id="q_water_heater" name="a[water_heater]" class="answer sl_answer">
+                <option>Gas</option>
+                <option>Electric</option>
+            </select>
         </div>
         <div class="submit"><div id="err"></div><input id="q_ajaxsubmit" name="q_ajaxsubmit" class="submit_button" type="submit" value="Submit"/></div>
     </form>
