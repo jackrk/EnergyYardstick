ce2e19e80ab71f1590a134d967780850d01e7b74
jsaun <jsaun@iastate.edu>
2014-11-30 19:54:57 -0600

    Finished rating function

--- web/controllers/rating.php
+++ web/controllers/rating.php
@@ -14,30 +14,81 @@ class controller_rating {
 		$stmt->bindParam(':username', $user_id);
 		$stmt->execute();
 
-		$usage = $stmt->fetch();
+		$usage = $stmt->fetch()[0];
 		
-		//Hardcoded till I figure out the SQL. 
-		$avg_usage = 500;
-		$max = 2000;
-		$people = 2;
+		$stmt = $dbh->prepare("SELECT * from House WHERE (house_id) = (:id)");
+        $stmt->bindParam(':id', $house_id );
+        $stmt->execute();
+        $house = $stmt->fetch();
+        $this_size = $house["size"];
+        $this_area = $house["neighborhood"];
+        $this_age = $house["year_built"];
+        $this_style = $house["house_style"];
+		
+		$lowerSizeBound = floor($this_size / 500) * 500 + 1;
+		$upperSizeBound = ceil($this_size / 500) * 500;
+		if ($this_size == 0) {
+			$lowerSizeBound = 0;
+			$upperSizeBound = 5000;
+		} else if ($this_size > 3000) {
+			$lowerSizeBound = 3000;
+			$upperSizeBound = 10000;
+		}
+		
+		if($this_age == 0){
+			$lowerAgeBound = 0;
+			$upperAgeBound = 3000;
+		}
+		else {
+			$range = floor($this_age / 20);
+			if($range < 97){
+				$lowerAgeBound = 0;
+				$upperAgeBound = 1940;
+			} else if($range == 97){
+				$lowerAgeBound = 1941;
+				$upperAgeBound = 1960;
+			} else if($range == 98) {
+				$lowerAgeBound = 1961;
+				$upperAgeBound = 1980;
+			} else if($range == 99){
+				$lowerAgeBound = 1981;
+				$upperAgeBound = 2000;
+			} else {
+				$lowerAgeBound = 2001;
+				$upperAgeBound = 3000;
+			}
+		}
+			
+		$stmt = $dbh->prepare("SELECT AVG(energy_usage) from EnergyUsage WHERE house_id IN (SELECT house_id FROM House WHERE size BETWEEN ? AND ? AND year_built BETWEEN ? AND ? ) AND energy_usage != 0");
+		$stmt->execute(array($lowerSizeBound, $upperSizeBound, $lowerAgeBound, $upperAgeBound));
+		$avg_usage = $stmt->fetch()[0];
+		
+		$stmt = $dbh->prepare("SELECT MAX(a) FROM (SELECT house_id,  AVG(energy_usage) as a  from EnergyUsage WHERE house_id IN (SELECT house_id FROM House WHERE size BETWEEN ? AND ? AND year_built BETWEEN ? AND ? ) AND energy_usage != 0 GROUP BY house_id) AS T ");
+		$stmt->execute(array($lowerSizeBound, $upperSizeBound, $lowerAgeBound, $upperAgeBound));
+		$max = $stmt->fetch()[0];
+
+		$stmt = $dbh->prepare("SELECT num_people FROM Initial_Questions where house_id = (:house_id);");
+		$stmt->bindParam(':house_id', $house_id);
+		$people = $stmt->fetch()[0];
 		
 		//constants.
 		$ppl_per_house = 2.26;
 		$plug_load = 0.2;
 		$min = 0;
 
-        // Temporary UI hook up. once the rating part works then move the second and third lines to the end
-        $rating = 56;
-        $ajax->insert('hidden_rating_number', $rating, true);
-        $ajax->call("../ajax.php?tips/get/$user_id/$house_id");
-
-        // For some reason I think this line is causing an error... Not sure though. When I had the two UI lines at the end it wouldn't work.
 		$usage = ($ppl_per_house/$people)*$plug_load*$usage + ( 1 - $plug_load)*$usage;
-
 
         // Rating should be on 1 - 100 scale here because it is converted in the UI
         // Tips have values that are based on 1 - 100 scale so they can be integers less than 10
 		$rating = -1;
+
+		if($usage > $avg_usage){
+			$rating = 50 - floor(((($usage - $avg_usage) / ($max - $avg_usage)) / 2) * 100);
+		} else {
+			$rating = 100 - floor((($usage / $avg_usage) / 2) * 100);
+		}
+		
+		/* 1 - 10 rating, keeping this here for safe keeping 
 		if($usage > $avg_usage){
 			$interval = ($max - $avg_usage) / 5.0;
 			$rating = 5 - floor(($usage - $avg_usage) / $interval);
@@ -45,8 +96,10 @@ class controller_rating {
 			$interval = ($avg_usage - $min) / 5.0;
 			$rating = 10 - floor($usage / $interval);
 		}
-
-
+		*/
+		
+        $ajax->insert('hidden_rating_number', $rating, true);
+        $ajax->call("../ajax.php?tips/get/$user_id/$house_id");
     }
 
 }
